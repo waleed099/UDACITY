242941a70041f2098a966196269ead4310609c70
waleed099 <w.saeeed123@gmail.com>
2023-09-19 02:02:07 -0700

    first commit

---
+++ my_store/src/app/checkout-form/checkout-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartItem } from 'src/app/models/CartItem';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-checkout-form',
+  templateUrl: './checkout-form.component.html',
+  styleUrls: ['./checkout-form.component.scss']
+})
+export class CheckoutFormComponent implements OnInit {
+
+  @Input() cart: CartItem[] = [];
+  fullname: string = '';
+  address: string = '';
+  card: string = '';
+
+  constructor(private router: Router, private userService: UserService) { }
+
+  ngOnInit(): void { }
+
+  onSubmit() {
+    this.userService.save({
+      fullname: this.fullname,
+      address: this.address,
+      card: this.card,
+    });
+    this.router.navigateByUrl('/checked-out');
+  }
+
+}
